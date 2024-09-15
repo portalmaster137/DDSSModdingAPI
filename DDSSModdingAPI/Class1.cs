ddd039142674d8010b88504634993b6bf8333f75
Porta <portalmaster137@gmail.com>
2024-09-15 18:39:25 +1000

    Add initial project files and configuration

---
+++ DDSSModdingAPI/Class1.cs
@@ -0,0 +1,11 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(DDSSModdingAPI.ModdingAPI), "DDSSModdingAPI", "1.0.0", "Porta")]
+[assembly: MelonGame("StripedPandaStudios", "DDSS")]
+
+namespace DDSSModdingAPI;
+
+public class ModdingAPI : MelonMod
+{
+    
+}
---
+++ DDSSModdingAPI/DDSSModType.cs
@@ -0,0 +1,8 @@
+﻿namespace DDSSModdingAPI;
+
+public enum DDSSModType
+{
+    CLIENT_ONLY,
+    HOST_ONLY,
+    CLIENT_AND_HOST
+}
